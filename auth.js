1e51f367cf5ad4b2f4c5fd0738068ee35e5c184e
Neeraj <neerajkumarghildiyal@gmail.com>
2022-06-08 23:34:50 +0530

    My first commit

---
+++ auth.js
@@ -0,0 +1,10 @@
+// In nodejs every file is a module 
+
+const register = function (username ){
+
+    console.log(`User ${username} has been registred`); 
+    // user backtik we can join variables and strings 
+}
+
+
+module.exports = register; 
